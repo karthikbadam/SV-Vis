202dc32112aa3d752c1a70d017010d54c7cc877f
karthikbadam <sbadam@purdue.edu>
2015-07-10 16:06:09 -0400

    needs axis

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ var NUMBER_OF_POSSIBILITIES = 2;
 
 var weights = [5, 4, 3, 3, 2, 2, 1];
 
-var variableNames = ["Conference paper", "Student Volunteer at VIS", "Poster/D.C.", "Student Volunteer before", "Submission under review", "PhD student", "MS student"];
+var variableNames = ["Conference-paper", "VIS-volunteer", "Poster/D.C.", "Other-volunteer", "Submission", "PhD-student", "MS-student"];
 
 var color = d3.scale.linear()
-    .domain([0, 20])
-    .range(["pink", "steelblue"])
+    .domain([0, 10, 20])
+    .range(["red", "white", "green"])
     .interpolate(d3.interpolateLab);
 
 
@@ -29,7 +29,7 @@ function computeMatrix(n, m) {
 
     var possibilites = Math.pow(m, n);
 
-    
+
 
     if (isEven(n)) {
 
@@ -58,7 +58,7 @@ function computeMatrix(n, m) {
             var index = i * matrixCols + j;
 
             var sum = 0;
-            
+
             var randomVector = new Array(n);
 
             // Check for the variables
@@ -99,123 +99,150 @@ function vectorToText(vector) {
     for (var i = 0; i < NUMBER_OF_VARIABLES; i++) {
 
         if (vector[i] == 1) {
-            
+
             if (i == NUMBER_OF_VARIABLES - 1) {
-                
+
                 text += variableNames[i];
-                
+
             } else {
-                
-                text += variableNames[i] + " : ";   
-                
+
+                text += variableNames[i] + " ";
+
             }
         }
 
     }
-    
+
     return text;
 
 }
 
 $(document).ready(function () {
 
-            var matrix = computeMatrix(NUMBER_OF_VARIABLES, NUMBER_OF_POSSIBILITIES);
+    var matrix = computeMatrix(NUMBER_OF_VARIABLES, NUMBER_OF_POSSIBILITIES);
 
-            console.log(matrix);
+    console.log(matrix);
 
-            deviceWidth = $(document).width();
-            deviceHeight = $(document).height();
+    deviceWidth = $(document).width();
+    deviceHeight = $(document).height();
 
-            var margin = {
-                left: 400,
-                top: 10,
-                right: 10,
-                bottom: 10
-            };
+    var margin = {
+        left: 100,
+        top: 10,
+        right: 10,
+        bottom: 10
+    };
 
-            var width = deviceWidth * 0.99 - margin.left - margin.right;
-            var height = deviceHeight * 0.9 - margin.top - margin.bottom;
+    var width = deviceWidth * 0.99 - margin.left - margin.right;
+    var height = deviceHeight * 0.95 - margin.top - margin.bottom;
 
-            var cellWidth = width < height ? width / matrixCols : height / matrixRows;
+    var cellWidth = width < height ? width / matrixCols : height / matrixRows;
 
-            var svg = d3.select("body").append("svg")
-                .attr("id", "cVis")
-                .attr("class", "hierarchial")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-            var x = d3.scale.linear().range([0, width]);
-            var y = d3.scale.linear().range([0, height]);
-
-            x.domain([0, matrixCols - 1]);
-            y.domain([0, matrixRows - 1]);
-
-            var row = svg.selectAll(".row")
-                .data(matrix)
-                .enter().append("g")
-                .attr("class", "row")
-                .each(row);
+    var svg = d3.select("body").append("svg")
+        .attr("id", "cVis")
+        .attr("class", "hierarchial")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-            function row(row) {
-                var cell = d3.select(this).selectAll(".cell")
-                    .data(row.filter(function (d) {
-                        return d.value;
-                    }))
-                    .enter().append("rect")
-                    .attr("class", "cell")
-                    .attr("x", function (d) {
-                        return cellWidth * d.x;
-                    })
-                    .attr("y", function (d) {
-                        return cellWidth * d.y;
-                    })
-                    .attr("width", cellWidth)
-                    .attr("height", cellWidth)
-                    .style("fill-opacity", function (d) {
-                        return 1;
-                    })
-                    .style("fill", function (d) {
-                        return color(d.value);
-                    })
-                    .style("stroke", function (d) {
-                        return "white";
-                    })
-                    .style("stroke-size", function (d) {
-                        return "0.5px";
-                    })
-                    .append("title")
-                    .text(function (d) {
+    var x = d3.scale.linear().range([0, width]);
+    var y = d3.scale.linear().range([0, height]);
 
-                        return vectorToText(d.vector);
+    x.domain([0, matrixCols - 1]);
+    y.domain([0, matrixRows - 1]);
 
-                    });
-
-            }
+    var row = svg.selectAll(".row")
+        .data(matrix)
+        .enter().append("g")
+        .attr("class", "row")
+        .each(row);
 
 
-            function mouseover(p) {
+    function row(row) {
+        var cell = d3.select(this).selectAll(".cell")
+            .data(row.filter(function (d) {
+                return d.value;
+            }))
+            .enter()
+            .append("g")
+            .attr("transform", function (d) {
+                return "translate(" + cellWidth * d.x + "," + cellWidth * d.y + ")";
+            })
+            .attr("class", "cell");
 
-
-            }
-
-
-            //now drawing each text
-            for (var i = 0; i < NUMBER_OF_VARIABLES; i++) {
-                if (isEven(i)) {
-
-                    var index = Math.floor(i); 
-                        //add text in y
-
-                    } else {
-
-                        //add text in x
-
-
-                    }
-                }
-
+        cell.append("rect")
+            .attr("width", cellWidth)
+            .attr("height", cellWidth)
+            .style("fill-opacity", function (d) {
+                return 0.8;
+            })
+            .style("fill", function (d) {
+                return color(d.value);
+            })
+            .style("stroke", function (d) {
+                return "white";
+            })
+            .style("stroke-size", function (d) {
+                return "0.5px";
             });
+
+        cell.append("text")
+            .attr("width", cellWidth)
+            .attr("dx", function (d) {
+                return 5;
+            })
+            .attr("dy", function (d) {
+                return 1.5;
+            })
+            .text(function (d) {
+                return vectorToText(d.vector);
+            })
+            .call(wrap, cellWidth);
+    }
+
+
+    function wrap(text, width) {
+        text.each(function () {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 0.4, // ems
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                dx = parseFloat(text.attr("dx")),
+                tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dx", dx).attr("dy", dy + "em");
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dx", dx).attr("dy", ++lineNumber * 0.025 * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    }
+
+
+
+    //now drawing each text
+    for (var i = 0; i < NUMBER_OF_VARIABLES; i++) {
+        if (isEven(i)) {
+
+            var index = Math.floor(i);
+            //add text in y
+
+        } else {
+
+            //add text in x
+
+
+        }
+    }
+
+});
